860041901868f7803007f4057a09308e3c5f9f22
bgagl <31654516+bgagl@users.noreply.github.com>
2023-05-20 10:45:54 +0200

    NW double check and boil down

--- helpers/get_stim_list.py
+++ helpers/get_stim_list.py
@@ -13,7 +13,7 @@ def get_stims(lexicon = "./", word_length=[3,4,5], sep=",", colname = "type"):
 
     return w_list_part
 
-def create_cs_from_w(words, vowels_uc = 'AEIOUÄÖÜ', vowels_lc = 'aeiouäöü', consonants_uc = 'BCDFGHJKLMNPQRSTVWXYZ', consonants_lc = 'bcdfghjklmnpqrstvwxyz'):
+def create_cs_from_w(words, vowels_uc = 'AEIOU', vowels_lc = 'aeiou', consonants_uc = 'BCDFGHJKLMNPQRSTVWXYZ', consonants_lc = 'bcdfghjklmnpqrstvwxyz'):
     #'bcdfghjklmnpqrstvwxyz'
     new_word_list = []
     for word in words:
@@ -30,8 +30,8 @@ def create_cs_from_w(words, vowels_uc = 'AEIOUÄÖÜ', vowels_lc = 'aeiouäöü'
     return new_word_list
 
 def get_pw(word):
-    vowels_lc = ['a', 'e', 'i', 'o', 'u','a', 'e', 'i', 'o', 'u','a', 'e', 'i', 'o', 'u','a', 'e', 'i', 'o', 'u', 'ü', 'ä', 'ö']
-    vowels_uc = ['A', 'E', 'I', 'O', 'U','A', 'E', 'I', 'O', 'U','A', 'E', 'I', 'O', 'U','A', 'E', 'I', 'O', 'U', 'Ü', 'Ä', 'Ö']
+    vowels_lc = ['a', 'e', 'i', 'o', 'u']
+    vowels_uc = ['A', 'E', 'I', 'O', 'U']
     new_pw = ''
     for char in word:
         if char in vowels_uc:
@@ -42,18 +42,104 @@ def get_pw(word):
             new_pw += char
     return new_pw
 
+def get_nw(word, type = "PW"):
+    if type == "PW":
+        vowels_lc = ['a', 'e', 'i', 'o', 'u']
+        vowels_uc = ['A', 'E', 'I', 'O', 'U']
+        new_nw = ''
+        for char in word:
+            if char in vowels_uc:
+                new_nw += random.choice(vowels_uc)
+            elif char in vowels_lc:
+                new_nw += random.choice(vowels_lc)
+            else:
+                new_nw += char
+
+    elif type == "CS":
+        vowels_lc = ['a', 'e', 'i', 'o', 'u']
+        vowels_uc = ['A', 'E', 'I', 'O', 'U']
+        consonants_uc = 'BCDFGHJKLMNPQRSTVWXYZ'
+        consonants_lc = 'bcdfghjklmnpqrstvwxyz'
+        new_nw = ''
+        for char in word:
+            if char in vowels_uc:
+                new_nw += random.choice(consonants_uc)
+            elif char in vowels_lc:
+                new_nw += random.choice(consonants_lc)
+            else:
+                new_nw += char
+    return new_nw
+
 def create_pw_from_w(words):#, vowels_uc = 'AEIOUÄÖÜ', vowels_lc = 'aeiouäöü', consonants_uc = 'BCDFGHJKLMNPQRSTVWXYZ', consonants_lc = 'bcdfghjklmnpqrstvwxyz'):
     new_pws = []
     for word in words:
         modified_word = get_pw(word)
         #print(modified_word+" Baseword: "+word)
+        i = 0
+        while modified_word in new_pws:
+            i = i + 1
+            if i < 60:
+                modified_word = get_pw(word)
+            else:
+                modified_word = "XXXXX"
+                print(modified_word + " Baseword: " + word)
+        i = 0
         while modified_word in words:
-            modified_word = get_pw(word)
+            i = i + 1
+            if i < 60:
+                modified_word = get_pw(word)
+            else:
+                modified_word = "XXXXX"
+                print(modified_word + " Baseword: " + word)
+
         new_pws.append(modified_word)
         #print(modified_word+" Baseword: "+word)
 
     return new_pws
 
+
+def create_NW_from_w_doubleCheck(words, type):#, vowels_uc = 'AEIOUÄÖÜ', vowels_lc = 'aeiouäöü', consonants_uc = 'BCDFGHJKLMNPQRSTVWXYZ', consonants_lc = 'bcdfghjklmnpqrstvwxyz'):
+    new_pws = []
+    n = 0
+    for word in words:
+        i = 0
+        modified_word = get_nw(word, type=type)
+        while check_string_in_lists(modified_word, words, new_pws):
+            i = i + 1
+            if i < 600:
+                modified_word = get_nw(word, type=type)
+            else:
+                modified_word = "XXXXX"+word
+                n = n+1
+                print(modified_word + " Baseword: " + word + " N=", str(n))
+
+        new_pws.append(modified_word)
+
+    return new_pws
+
+def boil_down_NW(words, nwords, type):
+    new_pws = []
+    for non_word in nwords:
+        if "XXXXX" in non_word:
+            base_w = random.choice(list(words))
+            modified_word = get_nw(base_w, type)
+            i=0
+            while check_string_in_lists(modified_word, words, nwords):
+                i = i + 1
+                if i < 600:
+                    modified_word = get_nw(base_w, type)
+                else:
+                    modified_word = "XXXXX" + base_w
+                    n = n + 1
+                    print(modified_word + " BoilDown Baseword: " + base_w + " N=", str(n))
+
+            new_pws.append(modified_word)
+        else:
+            new_pws.append(non_word)
+
+    return new_pws
+def check_string_in_lists(my_string, list1, list2):
+    return my_string in list1 or my_string in list2
 
 def generate_train_test_list(train="Train", test="Test", train_prob=.2, k=1):
     options = [train, test]
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from re import compile
 
-from helpers.get_stim_list import get_stims, create_cs_from_w, create_pw_from_w, generate_train_test_list
+from helpers.get_stim_list import get_stims, generate_train_test_list, \
+    boil_down_NW, create_NW_from_w_doubleCheck
 
 if __name__ == '__main__':
     stims = get_stims(lexicon="/Users/bg/PycharmProjects/Stimuli_for_DL/lexica/SUBTLEX_BEN_RAW_umlautchange.csv",
@@ -15,36 +16,39 @@ if __name__ == '__main__':
     #stim_df = pd.DataFrame(stims_s)
     #stim_df["stims_upper"] = [str(x).upper() for x in stims_s]
     #stims_upper_wo_rep = list(set(stims_upper))
-    stims_s_wo_rep_vowel = [x for x in stims_s_ if any(vowel in x for vowel in 'AEIOUAEIOUAEIOUAEIOUÄÖÜaeiouaeiouaeiouaeiouöäü')]
+    stims_s_wo_rep_vowel = [x for x in stims_s_ if any(vowel in x for vowel in 'AEIOUaeiou')]
 
-    stims_s_wo_rep_vowel = set(stims_s_wo_rep_vowel)
+    tmp = [string.lower() for string in stims_s_wo_rep_vowel]
+    to_rem_words = ["lest","masst","lust","dur","lost","list","last"]
+    tmp = [item for item in tmp if item not in to_rem_words]
+    stims_s_wo_rep_vowel = set(tmp)
+    #lest masst lust dur lost list last
     print(len(stims_s_wo_rep_vowel))
 
     stim_df = pd.DataFrame(stims_s_wo_rep_vowel)
 #    stim_df["Word"] = stims_s_wo_rep_vowel
     stim_df.rename(columns={0: 'Word'}, inplace=True)
 
-    stim_df["CS_v1"] = create_cs_from_w(words=stims_s_wo_rep_vowel)
-    stim_df["CS_v2"] = create_cs_from_w(words=stims_s_wo_rep_vowel)
-    stim_df["CS_v3"] = create_cs_from_w(words=stims_s_wo_rep_vowel)
+    stim_df["CS_v1"] = create_NW_from_w_doubleCheck(words=stims_s_wo_rep_vowel, type="CS")
+    stim_df["CS_v1"] = boil_down_NW(words=stims_s_wo_rep_vowel, nwords=stim_df["CS_v1"], type="CS")
 
-    stim_df["PW_v1"] = create_pw_from_w(words=stims_s_wo_rep_vowel)
-    stim_df["PW_v2"] = create_pw_from_w(words=stims_s_wo_rep_vowel)
-    stim_df["PW_v3"] = create_pw_from_w(words=stims_s_wo_rep_vowel)
+    stim_df["CS_v2"] = create_NW_from_w_doubleCheck(words=stims_s_wo_rep_vowel, type="CS")
+    stim_df["CS_v2"] = boil_down_NW(words=stims_s_wo_rep_vowel, nwords=stim_df["CS_v2"], type="CS")
+
+    stim_df["CS_v3"] = create_NW_from_w_doubleCheck(words=stims_s_wo_rep_vowel, type="CS")
+    stim_df["CS_v3"] = boil_down_NW(words=stims_s_wo_rep_vowel, nwords=stim_df["CS_v3"], type="CS")
+
+    stim_df["PW_v1"] = create_NW_from_w_doubleCheck(words=stims_s_wo_rep_vowel, type="PW")
+    stim_df["PW_v1"] = boil_down_NW(words=stims_s_wo_rep_vowel, nwords=stim_df["PW_v1"], type="PW")
+
+    stim_df["PW_v2"] = create_NW_from_w_doubleCheck(words=stims_s_wo_rep_vowel, type="PW")
+    stim_df["PW_v2"] = boil_down_NW(words=stims_s_wo_rep_vowel, nwords=stim_df["PW_v2"], type="PW")
+
+    stim_df["PW_v3"] = create_NW_from_w_doubleCheck(words=stims_s_wo_rep_vowel, type="PW")
+    stim_df["PW_v3"] = boil_down_NW(words=stims_s_wo_rep_vowel, nwords=stim_df["PW_v3"], type="PW")
 
     stim_df["train_test"] = generate_train_test_list(train_prob=0.8, k=len(stims_s_wo_rep_vowel))
 
     stim_df.to_csv("/Users/bg/PycharmProjects/Stimuli_for_DL/output/words_pw_cs_tt.csv")
-    #stim_df_ = pd.DataFrame(cs)
-    #stim_df_["cond"] = "CS"
-    #stim_df_.rename(columns={'0': 'string'})
 
-    #pd.concat([stim_df_, stim_df]).to_csv("/Users/bg/PycharmProjects/Stimuli_for_DL/output/words_cs.csv")
-    #print(create_pw_from_w(words=stims_s_wo_rep_vowel[:20]))
-
-    #stim_df_ = pd.DataFrame(pw)
-    #stim_df_["cond"] = "PW"
-    #stim_df_.rename(columns={'0': 'string'})
-
-    #pd.concat([stim_df_, stim_df]).to_csv("/Users/bg/PycharmProjects/Stimuli_for_DL/output/words_pw__.csv")
 
